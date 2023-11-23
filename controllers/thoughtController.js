beaab2241def1a6a94092b61817b42df773bab42
TorySnopl <Paramedic064@gmail.com>
2023-11-23 00:31:50 -0800

    thought controller almost finished

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { User, Thought } = require('../models');
+const { Reaction, Thought, User } = require('../models');
 
 module.exports = {
 //Get all thoughts
@@ -26,11 +26,59 @@ async getSingleThought (req,res) {
         res.status(500).json(err);
     }
 },
+
 //Post to create a new thought(dont forget to push the created thoughts _id to the associated users thoughts array field)
+async createThought(req,res){
+    try {
+        const thought = await Thought.create(req.body);
+        const user = await User.findOne({_id: req.params.userId});
+
+        if (!user){
+            return res.status(404).json({message: "User not found"});
+        }
+
+        user.thoughts.push(thought._id);
+        user.save();
+
+        res.json(thought);
+    }catch (err){
+        console.error(err);
+        return res.status(500).json(err);
+    }
+},
 
 // Put to update a thought by its _id
+async updateThought(req,res){
+    try{
+        const thought = await Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$set: req.body}
+        );
+
+        if(!thought){
+            return res.status(404).json({message: "no thought found"})
+        }
+
+        res.json(thought);
+    }catch(err){
+        res.status(500).json(err);
+    }
+},
 
 //Delete a thought by its _id
+async deleteThought(req,res){
+    try{
+        const thought = await Thought.findOneAndDelete({_id: req.params.thoughtId})
+
+        if(!thought){
+            return res.status(404).json({message: "no thought found with that Id"})
+        }
+
+        res.json({message: "Thought deleted"})
+    }catch(err){
+        res.status(500).json(err);
+    }
+},
 
 // /api/thoughts/:thoughtId/reactions;
 //Post to create a reaction stored in a single thoughts reactions array
