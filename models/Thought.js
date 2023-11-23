64b1c39f7dfcfa604425c61873a68e1d5033f0a9
TorySnopl <Paramedic064@gmail.com>
2023-11-23 11:29:15 -0800

    fixed reactions array in thought model

--- models/Thought.js
+++ models/Thought.js
@@ -20,7 +20,7 @@ const thoughtSchema = new Schema({
         type: String,
         required: true,
     },
-    reactions: [Reaction],
+    reactions: [Reaction.schema],
 },
 {
     timestamps: true,
