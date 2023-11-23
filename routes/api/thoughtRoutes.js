85b23d7663de43a964f3d0373fdfe74c91bff9dd
TorySnopl <Paramedic064@gmail.com>
2023-11-23 11:32:11 -0800

    fixed thoughtRoutes delete route with correct method

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,6 +9,6 @@ router.post('/thoughts', thoughtController.createThought);
 router.put('/thoughts/:thoughtId', thoughtController.updateThought);
 router.delete('/thoughts/:thoughtId', thoughtController.deleteThought);
 router.post('/thoughts/:thoughtId/reactions', thoughtController.createReaction);
-router.delete('/thoughts/:thoughtId/reactions/:reactionId', thoughtController.removeReaction);
+router.delete('/thoughts/:thoughtId/reactions/:reactionId', thoughtController.deleteReaction);
 
 module.exports = router;
